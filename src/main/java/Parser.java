dfdbf7a7e39be3fbc2e3205cb19df3ddd826a7b2
Victoria Oberascher <v.oberascher@commend.com>
2017-07-12 09:55:40 +0200

    BackupAnalyser

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -8,18 +8,18 @@ import java.io.File;
  */
 public class Parser {
 
-    public static void parseBackupFile(String directoryName, String jsonData){
+    public static void parseBackupFile(String directoryName, String jsonData) {
 
         File directory = new File(directoryName);
 
         //get all the files from a directory
         File[] fList = directory.listFiles();
 
-        for (File file : fList){
+        for (File file : fList) { //for each = mit for schleife array durchlaufen und dann immer File file = fList[i]
 
 //            System.out.println(file.getName());
 
-            if(file.getName().endsWith(".Media.json")) {
+            if (file.getName().endsWith(".Media.json")) {
 
                 System.out.println("found Media");
                 Parser.parseJsonFile(jsonData);
@@ -27,23 +27,21 @@ public class Parser {
         }
     }
 
-    public static void parseJsonFile(String jsonData)
-    {
+    public static void parseJsonFile(String jsonData) {
         JSONObject obj = new JSONObject(jsonData);
         String deviceDescription = obj.getJSONObject("deviceDescription").getString("applicationName");
         System.out.println("deviceDescription: " + deviceDescription);
 
 
         JSONArray arr = obj.getJSONArray("entities");
-        for (int i = 0; i < arr.length(); i++)
-        {
+        for (int i = 0; i < arr.length(); i++) {
             String id = arr.getJSONObject(i).getString("id");
             System.out.println(i + ". entities-id: " + id);
 //            Store.storeData(id);
 
             String displayName = arr.getJSONObject(i).getString("displayName");
             System.out.println(i + ". entities-displayname: " + displayName);
-            Store.storeData(i, id +" " + displayName);
+            Store.storeData(i, id + " " + displayName);
 
         }
 
